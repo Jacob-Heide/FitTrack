a2a3fd2f8050783b4859adda5212c2674fb5d5cb
Jacob-Heide <heidej10@gmail.com>
2024-01-03 18:38:14 -0700

    updates

--- server/src/index.js
+++ server/src/index.js
@@ -3,27 +3,28 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-import dotenv from 'dotenv';
-dotenv.config();
-
 //Route imports
 import userRoutes from './routes/users.route.js';
 
-// //sensitive variables
-const DBusername = process.env.USERNAME;
-
-const DBpassword = process.env.PASSWORD;
-
 const app = express();
 
-app.use(express.json());
-app.use(bodyParser.json());
+
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 //Define routes
 app.use('/api', userRoutes)
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+// //sensitive variables
+const DBusername = process.env.USERNAME;
+
+const DBpassword = process.env.PASSWORD;
+
+
 
 //Database connection starts here
 const CONNECTION_URL = `mongodb+srv://${DBusername}:${DBpassword}@fittrackdb.djl0zsm.mongodb.net/?retryWrites=true&w=majority`;
